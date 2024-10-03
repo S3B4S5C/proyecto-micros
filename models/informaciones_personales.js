06bf64bc815868196f08c6caa451fc2f20e8f02d
Sebastian Claure Collao <sebastian75601012@outlook.com>
2024-10-03 18:54:38 -0400

    Terminado ✅

--- models/informaciones_personales.js
+++ models/informaciones_personales.js
@@ -1,49 +1,49 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('sqlite::memory:');
+const informacionModelo = (sequelize, DataTypes) => {
+  const informacionPersonal = sequelize.define(
+    'informaciones_personales',
+    {
+      // Model attributes are defined here
+      id_informacion: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false
+      },
+      nombre: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+      },
+      apellido: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+      }, 
+      correo: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+      },
+      sexo: {
+          type: DataTypes.STRING(32),
+          allowNull:true
+      },
+      fecha_de_nacimiento: {
+          type: DataTypes.DATEONLY,
+          allowNull:true
+      },
+      direccion: {
+          type: DataTypes.STRING(255),
+          allowNull:true
+      },
+      carnet: {
+          type: DataTypes.STRING(255),
+          allowNull:true
+      },
+    },
+    {
+      tableName: 'informaciones_personales',
+      timestamps: false
+    },
+  )
+  return informacionPersonal
+}
 
-const InformacionPersonal= sequelize.define(
-  'InformacionPersonal',
-  {
-    // Model attributes are defined here
-    id_informacion: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-      allowNull: false
-    },
-    nombre: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
-    },
-    apellido: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
-    }, 
-    correo: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
-    },
-    sexo: {
-        type: DataTypes.STRING(32),
-        allowNull:true
-    },
-    fecha_de_nacimiento: {
-        type: DataTypes.DATEONLY,
-        allowNull:true
-    },
-    direccion: {
-        type: DataTypes.STRING(255),
-        allowNull:true
-    },
-    carnet: {
-        type: DataTypes.STRING(255),
-        allowNull:true
-    },
-  },
-  {
-    tableName: 'informaciones_personales',
-    timestamps: false
-  },
-);
-
-module.exports = InformacionPersonal;
+export default informacionModelo
