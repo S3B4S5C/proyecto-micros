5248876c6f9b24c5c226187871b7cf151545d982
Sebastian Claure Collao <sebastian75601012@outlook.com>
2024-10-03 19:53:26 -0400

    Terminado ✅

--- models/turno.js
+++ models/turno.js
@@ -1,41 +1,40 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('sqlite::memory:');
-
-const Turno= sequelize.define(
-  'Turno',
-  {
-    // Model attributes are defined here
-    usuario_chofer: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
-        references: {
-            model: 'choferes',
-            key: 'usuario_chofer'
-        },
-    },
-    id_horario: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      allowNull: false,
-      references: {
-        model: 'horarios',
-        key: 'id_horario'
+const turnoModelo = (sequelize, DataTypes) => {
+  const turno = sequelize.define(
+    'turno',
+    {
+      usuario_chofer: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+          references: {
+              model: 'choferes',
+              key: 'usuario_chofer'
+          },
       },
-    },
-    id_micro: {
-        type: DataTypes.BIGINT,
+      id_horario: {
+        type: DataTypes.UUID,
         primaryKey: true,
         allowNull: false,
         references: {
-            model: 'micros',
-            key: 'id_micro'
+          model: 'horarios',
+          key: 'id_horario'
         },
+      },
+      id_micro: {
+          type: DataTypes.BIGINT,
+          primaryKey: true,
+          allowNull: false,
+          references: {
+              model: 'micros',
+              key: 'id_micro'
+          },
+      },
     },
-  },
-  {
-    tableName: 'turno',
-    timestamps: false
-  },
-);
+    {
+      tableName: 'turno',
+      timestamps: false
+    },
+  )
+  return turno
+}
 
-module.exports = Turno;
+export default turnoModelo
