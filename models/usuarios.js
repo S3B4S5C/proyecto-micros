1590cae9140d4a83522a12ed47d13ca2ee082a1d
Sebastian Claure Collao <sebastian75601012@outlook.com>
2024-10-03 18:54:01 -0400

    Terminado ✅

---
+++ models/usuarios.js
@@ -0,0 +1,37 @@
+
+const usuariosModelo = (sequelize, DataTypes)=> {
+  const usuario = sequelize.define(
+    'usuarios',
+    {
+      usuario: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true
+      },
+      contraseña: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      salt: {
+        type: DataTypes.STRING(),
+        allowNull: false,
+      },
+      idInformacion: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        field: 'id_informacion',
+        references: {
+          model: 'informaciones_personales',
+          key: 'id_informacion'
+        }
+      }
+    },
+    {
+      freezeTableName: true,
+      timestamps: false
+    }
+  )
+  return usuario
+}
+
+export default usuariosModelo
