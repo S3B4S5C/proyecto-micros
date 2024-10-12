ef4d5b31e897582c1c75c7c66cecd7b5d0384db1
Sebastian Claure Collao <sebastian75601012@outlook.com>
2024-10-12 11:55:42 -0400

    cookie xd

--- controllers/sesionController.js
+++ controllers/sesionController.js
@@ -57,8 +57,8 @@ export const login = async (req, res) => {
 
       res.cookie("token", token, {
         httpOnly: true, // Protege la cookie para que no pueda ser accedida por JavaScript en el navegador
-        secure: false, // Solo en HTTPS en producción
-        sameSite: "None", // Evita el envío de la cookie en solicitudes entre sitios (para mayor seguridad)
+        secure: true, // Solo en HTTPS en producción
+        sameSite: "strict", // Evita el envío de la cookie en solicitudes entre sitios (para mayor seguridad)
         maxAge: 24 * 60 * 60 * 1000, // Expira en 1 día (en milisegundos)
       });
       res.status(200).json({
