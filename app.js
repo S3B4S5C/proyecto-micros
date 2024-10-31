0cfd7b736b7e37e4321688e33694b598bf01164f
Joel Cuéllar <joelcuellar2311@gmail.com>
2024-10-31 02:17:06 -0400

    nubeeee para todooooooos

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "https://7q577mvq-5173.brs.devtunnels.ms"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://7q577mvq-5173.brs.devtunnels.ms", "https://microsfrontend.vercel.app/"], 
     credentials: true,
   }),
 );
